83b8da44a4c6888b5fbe5af111f7060bca539146
Sasha Rakhlinskii <rahlinskiysasha@gmail.com>
2019-06-22 15:25:52 +0300

    Lesson 1 exampls

---
+++ src/classwork/lesson1/MathOperatorsExample.java
@@ -0,0 +1,13 @@
+package classwork.lesson1;
+
+public class MathOperatorsExample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        int result = a + b;
+
+        System.out.println(result);
+
+    }
+}
